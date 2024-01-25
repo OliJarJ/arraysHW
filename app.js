285a12f4a60019d6ffda8bce59e481f3809819aa
Oliver <oliver.a.jarjosa@gmail.com>
2024-01-24 19:06:09 -0500

    push

---
+++ app.js
@@ -0,0 +1,45 @@
+//Addition
+var a = 10;
+var b = 20;
+function addTwo(num1, num2) {
+  return num1 + num2;
+}
+console.log(addTwo(a,b))
+
+//Subtract
+var a = 50;
+var b = 35;
+function subtractTwo(num1, num2) {
+  return num1 - num2;
+}
+console.log(subtractTwo(a,b))
+
+//Multiply
+var a = 25;
+var b = 5;
+function multiplyTwo(num1, num2) {
+  return num1 * num2;
+}
+console.log(multiplyTwo(a,b))
+
+//Division
+var a = 46;
+var b = 23;
+function divideTwo(num1, num2) {
+  return num1 / num2;
+}
+console.log(divideTwo(a,b))
+
+//JavaScript Functions
+
+function getAverage (a,b){
+    var average = (a + b) / 2;
+    console.log(average);
+    return average;
+}
+
+function alert(string) {
+
+}
+getAverage(7,8);
+alert("hello");
